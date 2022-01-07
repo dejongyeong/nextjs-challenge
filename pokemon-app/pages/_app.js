1a80fe19fd525b48b629c9bf2a4fb94bd7921fb2
dejongyeong <dejong.re2@gmail.com>
2022-01-07 22:04:23 +0000

    remove and clean unnecessary files

--- pokemon-app/pages/_app.js
+++ pokemon-app/pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>NextJS Coding Challenge</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="Cambrean Interview Coding Challenge"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
