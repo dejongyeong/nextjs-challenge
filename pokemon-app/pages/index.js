5fdc9b3c0ad3ed42a365f16aceaad24a95651277
dejongyeong <dejong.re2@gmail.com>
2022-01-08 03:43:37 +0000

    add message container for loading and error messages

--- pokemon-app/pages/index.js
+++ pokemon-app/pages/index.js
@@ -7,16 +7,6 @@ import SearchBar from './components/home/SearchBar';
 
 const Pokemons = () => {};
 
-const Wrapper = styled.div`
-  width: 90%;
-  margin: 3% auto;
-  color: #ffffff;
-  h1 {
-    letter-spacing: 0.2rem;
-    font-weight: bold;
-  }
-`;
-
 export default function Home() {
   const {
     status: pokemonStatus,
@@ -30,17 +20,40 @@ export default function Home() {
   } = useFetchCategories();
 
   return (
-    <Wrapper>
+    <MainWrapper>
       <h1>Pokemon Wiki</h1>
       {pokemonStatus === 'loading' || categoriesStatus === 'loading' ? (
-        'Loading...'
+        <MessageContainer>
+          <h3>Loading...</h3>
+        </MessageContainer>
       ) : pokemonError === 'error' || categoriesError === 'error' ? (
-        <span>Error: {error.message}</span>
+        <MessageContainer>
+          <span>Error: {error.message}</span>
+        </MessageContainer>
       ) : (
         <>
           <SearchBar categories={categories} />
         </>
       )}
-    </Wrapper>
+    </MainWrapper>
   );
 }
+
+const MainWrapper = styled.div`
+  width: 90%;
+  margin: 3% auto;
+  color: #ffffff;
+  border: 1px solid black;
+  h1 {
+    letter-spacing: 0.2rem;
+    font-weight: bold;
+  }
+`;
+
+const MessageContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 1.5em auto;
+`;
