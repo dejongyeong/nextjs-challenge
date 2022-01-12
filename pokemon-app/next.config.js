ae8f2267e95c9740e7d7c772cf1db395e6bd51fc
dejongyeong <dejong.re2@gmail.com>
2022-01-12 00:14:27 +0000

    ignore linting during build

--- pokemon-app/next.config.js
+++ pokemon-app/next.config.js
@@ -3,4 +3,7 @@ module.exports = {
   images: {
     domains: ['raw.githubusercontent.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
--- pokemon-app/pages/_app.js
+++ pokemon-app/pages/_app.js
@@ -22,7 +22,11 @@ export default function MyApp({ Component, pageProps }) {
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={true}
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Average+Sans&family=Montserrat&family=Open+Sans&family=Raleway&family=Rubik:wght@300;400&display=swap"
           rel="stylesheet"
