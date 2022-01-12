36e58dd405651f3acac5d6e4a07371f4da0a277a
dejongyeong <dejong.re2@gmail.com>
2022-01-12 22:35:03 +0000

    add media query in evolution for handling more than 2 evolutions like eevee

--- pokemon-app/components/PokemonEvolutions.js
+++ pokemon-app/components/PokemonEvolutions.js
@@ -47,23 +47,32 @@ const EvolutionFormsWrapper = styled.div`
 const EvolutionForms = styled.div`
   display: flex;
   flex: 1 1 0px;
+  gap: 1.5em;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: auto;
-  margin: 2.2em auto;
+  margin: 1.8em auto;
+  padding: 0 0.7em;
+  @media (max-width: 1024.02px) {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  }
   @media (max-width: 320.02px) {
     display: inline-block;
   }
 `;
 
 const Evolution = styled.div`
+  width: 100%;
   margin: 0 auto;
   text-align: center;
   text-transform: capitalize;
   display: flex;
   flex-direction: column;
+  line-height: 1.3em;
   @media (max-width: 320.02px) {
+    display: block;
     &:not(:last-child) {
       margin-bottom: 2.5em;
     }
